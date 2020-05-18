8f7291c7f4602a46b67f8afa88c474709cadef23
Tony Lin <to_tonylin@163.com>
2020-05-18 16:31:30 -0400

    Minor bug fix.

--- models/modules/super_modules.py
+++ models/modules/super_modules.py
@@ -52,7 +52,7 @@ class SuperSeparableConv2d(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=in_channels * scale_factor, kernel_size=kernel_size,
                       stride=stride, padding=padding, groups=in_channels, bias=use_bias),
-            norm_layer(in_channels),
+            norm_layer(in_channels * scale_factor),
             nn.Conv2d(in_channels=in_channels * scale_factor, out_channels=out_channels,
                       kernel_size=1, stride=1, bias=use_bias),
         )
