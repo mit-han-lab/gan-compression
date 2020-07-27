f7fec657277f80d6b615d98568ca8182b92f9696
Lmxyy <lmxyy1999@foxmail.com>
2020-07-27 15:48:36 +0800

    fix typos

--- scripts/conda_deps.sh
+++ scripts/conda_deps.sh
@@ -3,4 +3,4 @@ set -ex
 conda install pytorch==1.4.0 torchvision==0.5.0 -c pytorch
 conda install tqdm scipy tensorboard
 conda install -c conda-forge tensorboardx
-pip install opencv-python dominate wget
+pip install opencv-python dominate wget torchprofile
--- scripts/cycle_gan/horse2zebra_lite/train_supernet.sh
+++ scripts/cycle_gan/horse2zebra_lite/train_supernet.sh
@@ -5,9 +5,9 @@ python train_supernet.py --dataroot database/horse2zebra \
   --log_dir logs/cycle_gan/horse2zebra_lite/supernet-stage1 \
   --gan_mode lsgan \
   --student_ngf 64 --ndf 64 \
-  --restore_teacher_G_path logs/pix2pix/edges2shoes-r_lite/mobile/checkpoints/latest_net_G.pth \
-  --restore_student_G_path logs/pix2pix/edges2shoes-r_lite/mobile/checkpoints/latest_net_G.pth \
-  --restore_D_path logs/pix2pix/edges2shoes-r_lite/mobile/checkpoints/checkpoints/latest_net_D.pth \
+  --restore_teacher_G_path logs/pix2pix/horse2zebra_lite/mobile/checkpoints/latest_net_G.pth \
+  --restore_student_G_path logs/pix2pix/horse2zebra_lite/mobile/checkpoints/latest_net_G.pth \
+  --restore_D_path logs/pix2pix/horse2zebra_lite/mobile/checkpoints/checkpoints/latest_net_D.pth \
   --real_stat_path real_stat/horse2zebra_B.npz \
   --lambda_recon 10 --lambda_distill 0.01 \
   --nepochs 50 --nepochs_decay 50 \
@@ -19,7 +19,7 @@ python train_supernet.py --dataroot database/horse2zebra \
   --log_dir logs/cycle_gan/horse2zebra_lite/supernet-stage2 \
   --gan_mode lsgan \
   --student_ngf 64 --ndf 64 \
-  --restore_teacher_G_path logs/pix2pix/edges2shoes-r_lite/mobile/checkpoints/latest_net_G.pth \
+  --restore_teacher_G_path logs/pix2pix/horse2zebra_lite/mobile/checkpoints/latest_net_G.pth \
   --restore_student_G_path logs/cycle_gan/horse2zebra_lite/supernet-stage1/checkpoints/latest_net_G.pth \
   --restore_D_path logs/cycle_gan/horse2zebra_lite/supernet-stage2/checkpoints/latest_net_D.pth \
   --real_stat_path real_stat/horse2zebra_B.npz \
