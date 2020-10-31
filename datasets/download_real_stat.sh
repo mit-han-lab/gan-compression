d39277333d562977acbaf0711f6ec67abae1c434
Lmxyy <lmxyy1999@foxmail.com>
2020-10-31 22:46:15 +0800

    add coco real_stat

--- datasets/download_real_stat.sh
+++ datasets/download_real_stat.sh
@@ -2,7 +2,7 @@
 
 echo "The real state files are in the format dataset_side.npz. Please specify the dataset and the side."
 echo "Supported file: cityscapes-A.npz edges2shoes_B.npz edges2shoes-r_B.npz edges2handbags_B.npz edges2handbags-r_B.npz"
-echo "                facades_A.npz horse2zebra_A.npz horse2zebra_B.npz maps_A.npz maps_B.npz"
+echo "                facades_A.npz horse2zebra_A.npz horse2zebra_B.npz maps_A.npz maps_B.npz coco_A.npz"
 
 FILE=$1
 SIDE=$2
