1e3a1dbef13367e168a523a669912d9587ee78c5
Lmxyy <lmxyy1999@foxmail.com>
2020-12-03 12:18:14 +0800

    fix a typo

--- scripts/pix2pix/edges2shoes-r_lite/finetune.sh
+++ scripts/pix2pix/edges2shoes-r_lite/finetune.sh
@@ -2,7 +2,7 @@
 python train_supernet.py --dataroot database/edges2shoes-r \
   --supernet resnet \
   --log_dir logs/pix2pix/edges2shoes-r_lite/finetune \
-  --batch_size 4 \
+  --batch_size 4 --teacher_netG resnet_9blocks \
   --restore_teacher_G_path pretrained/pix2pix/edges2shoes-r/full/latest_net_G.pth \
   --restore_student_G_path logs/pix2pix/edges2shoes-r_lite/supernet/checkpoints/latest_net_G.pth \
   --restore_D_path logs/pix2pix/edges2shoes-r_lite/supernet/checkpoints/latest_net_D.pth \
