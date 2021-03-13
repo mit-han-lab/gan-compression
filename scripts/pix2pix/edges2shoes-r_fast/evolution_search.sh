b4eb02f942069c558230e9cc24170c4b7532ca98
seekingdeep <muharraq@outlook.com>
2021-03-14 00:45:15 +0300

    Update evolution_search.sh

--- scripts/pix2pix/edges2shoes-r_fast/evolution_search.sh
+++ scripts/pix2pix/edges2shoes-r_fast/evolution_search.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python evolution_search.py --phase --dataroot database/edges2shoes-r \
+python evolution_search.py --phase train --dataroot database/edges2shoes-r \
   --restore_G_path logs/pix2pix/edges2shoes-r_fast/supernet/checkpoints/latest_net_G.pth \
   --output_dir logs/pix2pix/edges2shoes-r_fast/supernet/evolution \
   --ngf 64 --batch_size 32 \
   --config_set channels-64-pix2pix \
-  --real_stat_path real_stat/edges2shoes-r_subtrain_B.npz --load_in_memory \
+  --real_stat_path real_stat/edges2shoes-r_B.npz --load_in_memory \
   --budget 4.9e9 --mutate_prob 0.3 --meta_path datasets/metas/edges2shoes-r/train2.meta
