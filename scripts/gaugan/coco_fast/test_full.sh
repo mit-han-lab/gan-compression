559d2b50b1b345375db9a858a12723402cf25f4d
Lmxyy <lmxyy1999@foxmail.com>
2021-09-18 15:16:16 -0400

    fix a command

--- scripts/gaugan/coco_fast/test_full.sh
+++ scripts/gaugan/coco_fast/test_full.sh
@@ -3,7 +3,6 @@ python test.py --dataroot database/coco_stuff \
   --model spade --dataset_mode coco \
   --results_dir results-pretrained/gaugan/coco/full \
   --ngf 64 --netG spade \
-  --restore_G_path logs/gaugan/coco/full/checkpoints/latest_net_G.pth \
+  --restore_G_path pretrained/gaugan/coco_fast/full/latest_net_G.pth \
   --real_stat_path real_stat/coco_A.npz --table_path datasets/table.txt \
-  --need_profile --norm_G spectralspadesyncbatch3x3 \
-  --num_upsampling_layers normal
+  --need_profile --num_upsampling_layers normal
